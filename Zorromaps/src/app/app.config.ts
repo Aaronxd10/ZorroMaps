2a1caf5f33111981a85db69c5dbe359b0827e324
EmmanuelAbif <78711128+EmmanuelAbif@users.noreply.github.com>
2024-05-26 13:26:32 -0600

    quitar firebase

--- Zorromaps/src/app/app.config.ts
+++ Zorromaps/src/app/app.config.ts
@@ -3,15 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { FIREBASE_OPTIONS } from '@angular/fire/compat/firebase.app.module';
-import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig},
-    provideRouter(routes), 
-    provideAnimationsAsync(),
-    
-  ]
-
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
 };
--- Zorromaps/src/app/app.module.ts
+++ Zorromaps/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { initializeApp } from "firebase/app";
-import { environment } from './environments/environment';
 
-
-
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UserComponent } from './user/user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    //CrearCuentaComponent,
+    UserComponent,
+    UpdatepopupComponent,
+    CustomerComponent
+  ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase con la configuración
-    AngularFireDatabaseModule, // Importa el módulo de base de datos
-    AngularFirestoreModule,
+    CrearCuentaComponent,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
-  bootstrap: []
 })
 export class AppModule { }
