a5890a318be7b0673bbf23f405b30b87325a2383
EmmanuelAbif <78711128+EmmanuelAbif@users.noreply.github.com>
2024-05-26 12:58:36 -0600

    eliminar

--- Zorromaps/src/app/app.module.ts
+++ Zorromaps/src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { UserComponent } from './user/user.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
-import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
-import { CustomerComponent } from './customer/customer.component';
+
 
 @NgModule({
-  declarations: [
-    //CrearCuentaComponent,
-    UserComponent,
-    UpdatepopupComponent,
-    CustomerComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     CrearCuentaComponent,
