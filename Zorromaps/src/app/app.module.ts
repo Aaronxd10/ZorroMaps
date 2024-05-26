ad56669f559630a01c5be77dcef464706c402833
EmmanuelAbif <78711128+EmmanuelAbif@users.noreply.github.com>
2024-05-25 22:17:52 -0600

    firebase3.0

--- Zorromaps/src/app/app.module.ts
+++ Zorromaps/src/app/app.module.ts
@@ -1,40 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { UserComponent } from './user/user.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
-import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
-import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
-import { CustomerComponent } from './customer/customer.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { initializeApp } from "firebase/app";
-import { enviroment } from './environments/environment';
+import { environment } from './environments/environment';
+
+
 
 
 @NgModule({
-  declarations: [
-    UserComponent,
-    UpdatepopupComponent,
-    CustomerComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
-    CrearCuentaComponent,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    HttpClientModule,
-    ToastrModule.forRoot()
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase con la configuración
+    AngularFireDatabaseModule, // Importa el módulo de base de datos
+    AngularFirestoreModule,
   ],
   providers: [],
+  bootstrap: []
 })
 export class AppModule { }
