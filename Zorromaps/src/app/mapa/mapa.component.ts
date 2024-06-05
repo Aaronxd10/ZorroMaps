8531bdf14743bcbcf035555df6ca73217bd279b2
Aaronxd10 <117193713+Aaronxd10@users.noreply.github.com>
2024-06-05 13:04:26 -0600

    Conectado a la base de datos, se jalan los marcadores

--- Zorromaps/src/app/mapa/mapa.component.ts
+++ Zorromaps/src/app/mapa/mapa.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { AuthenticateService } from '../services/authenticate.service';
 import * as mapboxgl from 'mapbox-gl';
 import { MAPBOX_ACCESS_TOKEN } from '../mapa/mapa.config'; // Ajusta la ruta si es necesario
-
+import { AngularFireDatabase } from '@angular/fire/compat/database'; // Importa AngularFireDatabase
 
 
 @Component({
@@ -17,13 +17,20 @@ import { MAPBOX_ACCESS_TOKEN } from '../mapa/mapa.config'; // Ajusta la ruta si 
 })
 export class MapaComponent {
 
-  constructor(private router: Router, private authService: AuthenticateService){}
+
+  constructor(private router: Router, private authService: AuthenticateService, private db: AngularFireDatabase){}
 
   sugerencias: string[] = [];
   map: mapboxgl.Map | null = null;  // Inicialización de la propiedad
-
+  marcadores: {
+    name: string,
+    coordinates: [number, number],
+    intermediatePoints?: [number, number][],
+    steps?: string[]
+  }[] = [];
 
   ngOnInit(): void {
+    // Inicializa el mapa
     this.map = new mapboxgl.Map({
       container: 'map', // ID del contenedor en tu HTML
       style: 'mapbox://styles/19141218/clwile9wp01gk01qo11vu2y9f', // Estilo del mapa
@@ -35,7 +42,10 @@ export class MapaComponent {
 
     this.map.on('load', () => {
       this.defineZone();
-      this.agregarMarcadores();
+      this.db.list('/marcadores').valueChanges().subscribe((data: any) => {
+        this.marcadores = data;
+        this.agregarMarcadores();
+      });
     });
   }
 
@@ -69,54 +79,25 @@ export class MapaComponent {
       }
     });
   }
-  marcadores: { name: string, coordinates: [number, number] }[] = [
-    { name: "Direccion", coordinates: [-100.40552259718322, 20.593618079982686] },
-    { name: "Servicios Escolares", coordinates: [-100.40540409851523, 20.593351708226223] },
-    { name: "Cancha", coordinates: [-100.40581469542131, 20.594908065032868] },
-    { name: "Centro De Informacion", coordinates: [-100.4051567928475, 20.59292712590462] },
-    { name: "Centro de Copiado", coordinates: [-100.40522237138502, 20.593186511881363] },
-    { name: "Cafeteria", coordinates: [-100.40535245821307, 20.59398118359892] },
-    { name: "Auditorio", coordinates: [-100.4059640024669, 20.594140620238107] },
-    { name: "Centro de Idiomas", coordinates: [-100.4043506538542, 20.593092353546716] },
-    { name: "Laboratorio de Quimica", coordinates: [-100.40469074917935, 20.592944184529394] },
-    { name: "Edificio F", coordinates: [-100.40480931069172, 20.593824258328585] },
-    { name: "Edificio B", coordinates: [-100.40456388841513, 20.593717548136244] },
-    { name: "Edificio C", coordinates: [-100.40462705781555, 20.59345107234265] },
-    { name: "Edificio G", coordinates: [-100.40470487143143, 20.594110864419093] },
-    { name: "Laboratorio de Matematicas", coordinates: [-100.40427407821244, 20.594054913624632] },
-    { name: "Edificio J", coordinates: [-100.40568035769766,20.594652418585216] },
-    { name: "Division de Estudios Profesionales", coordinates: [-100.40545142862305, 20.59446451062098] },
-    { name: "Edificio I", coordinates: [-100.40488956208807, 20.594804422018946] },
-    { name: "Laboratorio de Electronica", coordinates: [-100.40499778557738, 20.595067274250745] },
-    { name: "Alberca", coordinates: [-100.40531368313798, 20.5949960853072] },
-    { name: "Laboratorio de Ingenieria Industrial", coordinates: [-100.40450492859108, 20.59484275458468] },
-    { name: "Centro de Computo", coordinates: [-100.40519522135426, 20.595490299684798] },
-    { name: "Edificio K", coordinates: [-100.40466330571326, 20.59541822747681] },
-    { name: "Edificio X", coordinates: [-100.40463767551115, 20.595209011578216] },
-    { name: "Edificio H", coordinates: [-100.40471366043532, 20.594369946665356] },
-    { name: "Sala Audiovisual 2", coordinates: [-100.40447218034348, 20.594445547425384] },
-    { name: "Sala Audiovisual 3", coordinates: [-100.40434376905252, 20.593775720251145] },
-
-    ];
 
   agregarMarcadores() {
+
     new mapboxgl.Marker({ color: 'green' })
     .setLngLat([-100.405979, 20.593216])
     .setPopup(new mapboxgl.Popup().setHTML("<h3>Inicio</h3>"))
     .addTo(this.map!);
 
     this.marcadores.forEach(marcador => {
-    const marker = new mapboxgl.Marker()
-      .setLngLat(marcador.coordinates)
-      .setPopup(new mapboxgl.Popup().setHTML(`<h3>${marcador.name}</h3>`))
-      .addTo(this.map!);
+      const marker = new mapboxgl.Marker()
+        .setLngLat(marcador.coordinates)
+        .setPopup(new mapboxgl.Popup().setHTML(`<h3>${marcador.name}</h3>`))
+        .addTo(this.map!);
 
-    // Agregar un evento de clic a cada marcador
-    marker.getElement().addEventListener('click', () => {
-      this.calcularRuta(marcador.coordinates); // Llama a la función calcularRuta con las coordenadas del marcador clickeado
+      // Agregar un evento de clic a cada marcador
+      marker.getElement().addEventListener('click', () => {
+        this.calcularRuta(marcador.coordinates); // Llama a la función calcularRuta con las coordenadas del marcador clickeado
+      });
     });
-  });
-
   }
 
   calcularRuta(destino: [number, number] | string) {
@@ -174,7 +155,6 @@ export class MapaComponent {
     });
   }
 
-
   @ViewChild('busquedaInput') busquedaInput!: ElementRef<HTMLInputElement>;
 
   buscar() {
@@ -215,7 +195,6 @@ export class MapaComponent {
     this.authService.logout();
     this.router.navigate(['inicio']);
   }
-
 }
 
 
