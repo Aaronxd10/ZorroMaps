e7b63cc93f4beee29e0fd0def5bf986417be55da
Aaronxd10 <117193713+Aaronxd10@users.noreply.github.com>
2024-06-04 18:17:56 -0600

    Mapa montado en angular

--- Zorromaps/src/app/mapa/mapa.component.ts
+++ Zorromaps/src/app/mapa/mapa.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthenticateService } from '../services/authenticate.service';
+import * as mapboxgl from 'mapbox-gl';
+import { MAPBOX_ACCESS_TOKEN } from '../mapa/mapa.config'; // Ajusta la ruta si es necesario
+
 
 @Component({
   selector: 'app-mapa',
@@ -9,13 +12,27 @@ import { AuthenticateService } from '../services/authenticate.service';
   imports: [ CommonModule],
   templateUrl: './mapa.component.html',
   styleUrl: './mapa.component.css',
-  
+
 })
 export class MapaComponent {
 
   constructor(private router: Router, private authService: AuthenticateService){}
 
   sugerencias: string[] = [];
+  map: mapboxgl.Map | null = null;  // Inicialización de la propiedad
+
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: 'map', // ID del contenedor en tu HTML
+      style: 'mapbox://styles/19141218/clwile9wp01gk01qo11vu2y9f', // Estilo del mapa
+      center: [-100.404873, 20.593692], // Coordenadas del Instituto Tecnológico de Querétaro
+      zoom: 17,
+      pitch: 50,
+      accessToken: MAPBOX_ACCESS_TOKEN // Configura el token de acceso aquí
+    });
+  }
+
+
 
   lugares = [
     "Salones",
@@ -44,10 +61,11 @@ export class MapaComponent {
     input.value = sugerencia;
     this.sugerencias = [];
   }
-  
+
   logout(){
     this.authService.logout();
     this.router.navigate(['inicio']);
   }
 
 }
+
---
+++ Zorromaps/src/app/mapa/mapa.config.ts
@@ -0,0 +1,2 @@
+// src/app/mapbox-config.ts
+export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiMTkxNDEyMTgiLCJhIjoiY2x3aWxiajFrMGQ4bDJqbnIyc3U2dXh2dSJ9.F7-tPZXbjdg3GbCeDULKWg';
